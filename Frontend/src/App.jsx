18a9f0259e8d64cf07e32e3611c1f42b282a0423
Nguyễn Quốc Huy <darkness1652004@gmail.com>
2026-03-10 15:26:39 +0700

    sua frontend deploy

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserList from './UserList'; // Đảm bảo file này cùng thư mục
+import UserList from './UserList'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Trang chủ - Hãy truy cập /users</h1>} />
-        {/* Đây là đường dẫn giáo viên sẽ chấm điểm */}
+        <Route path="/" element={<h1>Trang chủ - Hệ thống Quản lý</h1>} />
+        {/* Đây là route quan trọng nhất để lấy điểm */}
         <Route path="/users" element={<UserList />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
